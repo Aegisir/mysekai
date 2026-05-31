0a37bbc977506838871216cb060374840c003e4f
JustKanade <muxian0219@qq.com>
2026-05-31 21:39:16 +0800

    feat(config): set base path for GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'node:url';
 const srcPath = fileURLToPath(new URL('./src', import.meta.url));
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/mysekai/' : '/',
   plugins: [solid()],
   resolve: {
     alias: {
